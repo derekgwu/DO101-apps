a4acd035f396146699a95a77a5cf69bdc446a3bb
Derek Chen <dchen36@gwmail.gwu.edu>
2024-10-12 18:48:28 -0400

    change for real

--- version/app.js
+++ version/app.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
   response = 'This is version 3 of the app.' + '\n';
 
   //send the response to the client
-  res.send(response);
+  res.send(response); 
 
 });
 
